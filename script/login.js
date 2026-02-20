61506778d9e4c18d6bc2ddf34a79107596b2a83c
md-sajid-sarkar <25549011089@student.bup.edu.bd>
2026-02-20 22:48:02 +0600

    first commit

--- script/login.js
+++ script/login.js
@@ -18,7 +18,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
         // 3-1 true>> alert >homepage
         alert("Login Success!");
         // window.location.replace("/home.html");
-        window.location.assign("/home.html");
+        window.location.assign("./home.html");
     }
     else {
         // 3-2 false>> alert > return
